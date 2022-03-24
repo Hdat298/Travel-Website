d13d90c4947ed7c915810963b5df283d74ae26e2
Hdat298 <hdat318@gmail.com>
2022-03-24 19:06:55 +0700

    admin-login-form

--- Travel-Website/Scripts/script.js
+++ Travel-Website/Scripts/script.js
@@ -7,6 +7,24 @@ let menu = document.querySelector('#menu-bar');
 let navbar = document.querySelector('.navbarr');
 let videoBtn = document.querySelectorAll('.vid-btn');
 
+const inputs = document.querySelectorAll('.input');
+
+function focusFunc() {
+    let parent = this.parentNode.parentNode;
+    parent.classList.add('focus');
+}
+function blurFunc() {
+    let parent = this.parentNode.parentNode;
+    if (this.value == "") {
+        parent.classList.remove('focus');
+    }
+}
+
+inputs.forEach(input => {
+    input.addEventListener('focus', focusFunc);
+    input.addEventListener('blur', blurFunc);
+});
+
 window.onscroll = () => {
     searchBtn.classList.remove('fa-times');
     searchBar.classList.remove('active');
