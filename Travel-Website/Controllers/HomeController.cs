ea241f55214197d183ab2dc1227f09f6475ded89
Sioldil <acwuy2@gmail.com>
2022-03-26 22:06:59 +0700

    update

--- Travel-Website/Controllers/HomeController.cs
+++ Travel-Website/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Travel_Website.Models;
+using System.Dynamic;
 
 namespace Travel_Website.Controllers
 {
@@ -10,7 +12,31 @@ namespace Travel_Website.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            dynamic dy = new ExpandoObject();
+            dy.hinhanhttlist = getHinhAnhTinhThanh();
+            dy.tinhthanhlist = getTinhThanh();
+            return View(dy);
+        }
+
+        public List<HinhAnhTinhThanh> getHinhAnhTinhThanh()
+        {
+            DataClasses1DataContext context = new DataClasses1DataContext();
+            List<HinhAnhTinhThanh> tinhThanhs = context.HinhAnhTinhThanhs.ToList();
+            return tinhThanhs;
+        }
+
+        public List<TinhThanh> getTinhThanh()
+        {
+            DataClasses1DataContext context = new DataClasses1DataContext();
+            List<TinhThanh> tinhThanhs = context.TinhThanhs.ToList();
+            return tinhThanhs;
+        }
+
+        public List<Tour> getTour()
+        {
+            DataClasses1DataContext context = new DataClasses1DataContext();
+            List<Tour> tinhThanhs = context.Tours.ToList();
+            return tinhThanhs;
         }
 
         public ActionResult Signup()
--- Travel-Website/Controllers/ImagesTController.cs
+++ Travel-Website/Controllers/ImagesTController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Travel_Website.Models;
+using System.Web.UI.WebControls;
+using System.IO;
 
 namespace Travel_Website.Controllers
 {
@@ -23,25 +25,29 @@ namespace Travel_Website.Controllers
             HinhAnhTinhThanh p = context.HinhAnhTinhThanhs.FirstOrDefault(x => x.ID == id);
             return View(p);
         }
-
-        public ActionResult Create(HttpPostedFileBase file)
+  
+        public ActionResult Create(HttpPostedFileBase HinhAnh1)
         {
             if (Request.Form.Count>0)
             {
+
                 DataClasses1DataContext context = new DataClasses1DataContext();
                 HinhAnhTinhThanh hinhAnh = new HinhAnhTinhThanh();
+                
                 hinhAnh.MaTinhThanh = int.Parse(Request.Form["MaTinhThanh"]);
-                hinhAnh.MoTa = Request.Form["MoTa"];             
+                hinhAnh.MoTa = Request.Form["MoTa"];
 
-                if(file != null)
+                byte[] bytes;
+                using (BinaryReader br = new BinaryReader(HinhAnh1.InputStream))
                 {
-                   hinhAnh.HinhAnh = new byte[file.ContentLength];
-                   file.InputStream.Read(hinhAnh.HinhAnh.ToArray(), 0, file.ContentLength);
+                    bytes = br.ReadBytes(HinhAnh1.ContentLength);
                 }
+
+                hinhAnh.HinhAnh = bytes;
 
                 context.HinhAnhTinhThanhs.InsertOnSubmit(hinhAnh);
                 context.SubmitChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("Index"); 
             }
             return View();
         }
@@ -67,6 +73,18 @@ namespace Travel_Website.Controllers
                 }
                 context.SubmitChanges();
                 return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            DataClasses1DataContext context = new DataClasses1DataContext();
+            HinhAnhTinhThanh tinhThanh = context.HinhAnhTinhThanhs.FirstOrDefault(x => x.ID == id);
+            if (tinhThanh != null)
+            {
+                context.HinhAnhTinhThanhs.DeleteOnSubmit(tinhThanh);
+                context.SubmitChanges();
+            }
+            return RedirectToAction("Index");
         }
     }
 }
