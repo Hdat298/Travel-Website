ebb56ea3441139bfaeb6ebe75b9e090170ae3711
Hdat298 <hdat318@gmail.com>
2022-04-09 18:32:42 +0700

    add css

---
+++ Travel-Website/Scripts/script1.js
@@ -0,0 +1,31 @@
+﻿let formBtn = document.querySelector('#login-btn')
+let loginForm = document.querySelector('.login-form-container')
+let formClose = document.querySelector('#form-close')
+let forgotPassBtn = document.querySelector('.forgotPassword')
+let forgotPassForm = document.querySelector('.forgotPass-form-container')
+let formClose1 = document.querySelector('#form-close1')
+let userMenu = document.querySelector('.profile');
+let toggleMenu = document.querySelector('.menu');
+if (userMenu != null) {
+    userMenu.addEventListener('click', () => {
+        toggleMenu.classList.toggle('active');
+    });
+}
+else {
+    userMenu = null;
+}
+
+formBtn.addEventListener('click', () => {
+    loginForm.classList.add('active');
+});
+
+formClose.addEventListener('click', () => {
+    loginForm.classList.remove('active');
+});
+forgotPassBtn.addEventListener('click', () => {
+    forgotPassForm.classList.add('active');
+});
+
+formClose1.addEventListener('click', () => {
+    forgotPassForm.classList.remove('active');
+});
